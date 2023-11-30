931d8108241812f02a3602e00cdd004d18548d4d
EmilianaAP <61345598+EmilianaAP@users.noreply.github.com>
2023-11-30 16:49:45 +0200

    Add files via upload
    App with main, login and register pages (Without functionalities)

---
+++ Dog-feeder/Dog-feeder/ContentView.swift
@@ -0,0 +1,143 @@
+//
+//  ContentView.swift
+//  Dog-feeder
+//
+//  Created by Emiliana Petrenko on 16.11.23.
+//
+
+import SwiftUI
+
+struct Login: View {
+@State private var username: String = ""
+@State private var password: String = ""
+    
+    var body: some View {
+        ZStack{
+            Image("Login-Register")
+                .padding(.bottom, 250)
+                .padding(.top, 30)
+            
+            VStack{
+                
+                Text("Login")
+                    .font(.custom(
+                            "Baskerville-BoldItalic",
+                            fixedSize: 36))
+                    .bold()
+                    .padding(.top, 20)
+                    .padding(.bottom, 10)
+                
+                Form {
+                    TextField(text: $username, prompt: Text("Username")) {
+                        Text("Username")
+                    }
+                    SecureField(text: $password, prompt: Text("Password")) {
+                        Text("Password")
+                    }
+                    
+                    Button("Login") {
+                        // Login!
+                    }
+                    .fixedSize()
+                    .foregroundColor(Color("Pink"))
+                }
+            }
+        }
+    }
+}
+
+struct Register: View {
+@State private var username: String = ""
+@State private var email: String = ""
+@State private var password: String = ""
+@State private var password_confirm: String = ""
+
+    
+    var body: some View {
+        VStack{
+            Text("Register")
+                .font(.custom(
+                        "Baskerville-BoldItalic",
+                        fixedSize: 36))
+                .bold()
+                .padding(.bottom, 10)
+            
+            Form {
+                TextField(text: $username, prompt: 
+                    Text("Username")) {
+                    Text("Username")
+                }
+                TextField(text: $email, prompt:
+                    Text("Email")) {
+                    Text("Email")
+                }
+                SecureField(text: $password, prompt:
+                    Text("Password")) {
+                    Text("Password")
+                }
+                SecureField(text: $password_confirm, prompt:
+                    Text("Confirm password")) {
+                    Text("Confirm password")
+                }
+                
+                Button("Submit") {
+                    // register!
+                }
+                .fixedSize()
+                .foregroundColor(Color("Pink"))
+                
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var showLogin = false
+    @State private var showRegister = false
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image("Login-Register")
+                    .padding(.bottom, 250)
+                    .padding(.top, 60)
+                VStack {
+                    Text("SnackBuddy")
+                        .font(.custom(
+                                "Baskerville-BoldItalic",
+                                fixedSize: 36))
+                        .bold()
+                        .padding(.top, 80)
+                        .padding(.bottom, 10)
+                    
+                    Button("Login") {
+                        print("Login tapped!")
+                        showLogin = true
+                    }
+                    .padding(.bottom, 5)
+                    .fixedSize()
+                    .foregroundColor(Color("Pink"))
+                    
+                    Button("Register") {
+                        print("Register tapped!")
+                        showRegister = true
+                    }
+                    .fixedSize()
+                    .foregroundColor(Color("Pink"))
+                }
+                
+                .navigationDestination(
+                     isPresented: $showLogin) {
+                          Login()
+                     }
+                .navigationDestination(
+                     isPresented: $showRegister) {
+                          Register()
+                     }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dog-feeder/Dog-feeder/Dog_feederApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_feederApp.swift
+//  Dog-feeder
+//
+//  Created by Emiliana Petrenko on 16.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Dog_feederApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
