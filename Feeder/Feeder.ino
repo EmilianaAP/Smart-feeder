1770fef2500bdeb036183870c39fa1e7b8345be1
EmilianaAP <ema.andr33va@gmail.com>
2024-01-11 02:45:21 +0200

    Esp file updated

--- Feeder/Feeder.ino
+++ Feeder/Feeder.ino
@@ -1,8 +1,48 @@
 #include <Wire.h>
 #include <RTClib.h>
 #include <WiFiManager.h>
+#include <SPI.h>
+#include <PubSubClient.h>
 
 RTC_DS3231 rtc;
+IPAddress server(34, 122, 107, 45);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String response;
+
+  for (int i = 0; i < length; i++)
+  {
+    response += (char)payload[i];
+  }
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  Serial.println(response);
+}
+
+WiFiClient espClient;
+PubSubClient client(server, 1883, callback, espClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("Feeder","hello world");
+      // ... and resubscribe
+      client.subscribe("Topic_2");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
   Wire.begin(5,4);
@@ -14,10 +54,38 @@ void setup() {
     while (1);
   }
 
+  WiFiManager wm;
+  bool res;
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("AutoConnectAP","password"); // password protected ap
+
+  if(!res) {
+      Serial.println("Failed to connect");
+      // ESP.restart();
+  } 
+  else {
+      //if you get here you have connected to the WiFi    
+      Serial.println("connected...yeey :)");
+  }
+
+
+  if (client.connect("arduinoClient", "Erix", "!!SmartPet!!")) {
+    client.publish("outTopic","hello world");
+    client.subscribe("inTopic");
+  }
+
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  delay(1500);
 }
 
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
   // put your main code here, to run repeatedly:
   DateTime now = rtc.now();
   Serial.print("ESP32 RTC Date Time: ");
