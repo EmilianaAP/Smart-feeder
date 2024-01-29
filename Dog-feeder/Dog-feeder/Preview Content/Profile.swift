fd88db3b1dd65815a3ce3b091a36d1c64e7fd8f1
EmilianaAP <ema.andr33va@gmail.com>
2024-01-29 15:04:12 +0200

    Changes to dog's profile page

--- Dog-feeder/Dog-feeder/Preview Content/Profile.swift
+++ Dog-feeder/Dog-feeder/Preview Content/Profile.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct Profile:View {
+    var name: String = "Erix"
+    var bread: String = "Snauchzer"
+    var age: String = "3"
+    var sex: String = "♂︎"
+    var weight: String = "20.5"
+    var location: String = "Sofia, Bulgaria"
+    
     var body: some View {
         ZStack{
             Color("Background").ignoresSafeArea(.all)
-            Text("Profile page")
+            VStack{
+                Text(name + " " + sex) //switch to dog name from firebase
+                    .bold()
+                    .font(.system(size: 36))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 10)
+                
+                Text("📍" + location)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding([.leading, .bottom], 10)
+                
+                HStack(spacing: 20) {
+                    Text("Age: \n" + age)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing], 20)
+                        .padding([.top, .bottom], 40)
+                        .background(Color("Buttons.Login-Register"))
+                        .foregroundColor(.white)
+                        .cornerRadius(22)
+                    
+                    Text("Bread: \n" + bread)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing], 20)
+                        .padding([.top, .bottom], 40)
+                        .background(Color("Buttons.Login-Register"))
+                        .foregroundColor(.white)
+                        .cornerRadius(22)
+                    
+                    Text("Weight: \n" + weight)
+                        .multilineTextAlignment(.center)
+                        .padding([.leading, .trailing], 20)
+                        .padding([.top, .bottom], 40)
+                        .background(Color("Buttons.Login-Register"))
+                        .foregroundColor(.white)
+                        .cornerRadius(22)
+                }
+            }
         }
     }
 }
